b92fecaf3f03cf46de8bb257f5b764d5aaab163f
jamesD33064 <james0120160379@gmail.com>
2022-03-16 14:31:49 +0800

    .

--- SystemProgram110-2/HW_2/ans_1.asm
+++ SystemProgram110-2/HW_2/ans_1.asm
@@ -2,27 +2,9 @@
 
 .COPY    START   1000
 .FIRST   JSUB    READ
-        JSUB    READ
         JSUB    WRITE
         J	OVER
 .       END     FIRST
-
-... READ
-READ    LDX     ZERO
-RLOOP   TD      INDEV
-        JEQ     RLOOP
-R       RD      INDEV   .讀一個 byte 到 A暫存
-
-        STL	FUNC    .紀錄READ的位址
-        JSUB    CHANGE  .L位址變這行
-        LDL	FUNC    .將L的位址改回READ
-        STCH    STR , X
-        
-        TIX	COIN    .加一直到讀到＄
-        COMP	COIN
-        JEQ     JUMP    .就跳走到RSUB
-
-        J	R
 
 ... WRITE
 WRITE   LDX	ZERO
@@ -35,21 +17,6 @@ W       LDCH	STR , X
         COMP	COIN
         JEQ	JUMP
         J	W
-
-... CHANGE TO BIG
-CHANGE  COMP	CHA
-	JLT	JUMP
-
-	COMP	CHZ
-	JGT	JUMP
-
-        SUB     C
-
-... RSUB
-JUMP    RSUB
-
-... OUT
-OVER    LDX	ZERO
 
 
 ...   ---------------------
