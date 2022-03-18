75e8a8a5e73892762ce5133edb724ba4696ae9d4
D33064 <james0120160379@gmail.com>
2022-03-19 04:58:10 +0800

    .

--- SystemProgram110-2/HW_2/ans_1.asm
+++ SystemProgram110-2/HW_2/ans_1.asm
@@ -1,45 +1,77 @@
-... MAIN
+HW2     START   0
+MAIN    JSUB	WRITE
+        . J       OUT
 
 ... WRITE 
 WRITE   LDX	#0
 WLOOP   TD	OUTDEV
         JEQ	WLOOP
 
+        JSUB    PS
+        JSUB    PS
+        JSUB    PS
+        LDA	#0
+
+UP      ADD	#1
+        JSUB    PS
+        JSUB    PS
+        ADD	#48
+        WD	OUTDEV
+        SUB     #48
+        COMP	#9
+        JLT	UP
+        LDA	NL
+        WD	OUTDEV
+
 CAC     LDS	#0
 SET     LDX	#0      .初始化
         LDA	#1      .S為外層參數
         ADDR	A,S     .將S加A (1)
+        
+        RMO     S,A
+        COMP	#10
+        JEQ	OUT
 
-IN      STA	PUTA    .Ａ為內層參數
+        JSUB	PRINT
+        LDA	#1      .S為外層參數
+
+IN      STA	ACOUNT  .Ａ為內層參數
 
         MULR	S,A     .相乘結果到Ａ
         STA	RESULT  .結果放入RESULT
 
-        J	PRINT   .把Ａ的東西印出來 
+        JSUB	PRINT   .把Ａ的東西印出來 
 BACK    
-        LDA	PUTA    .將A還原
-        ADD	#1     .A加一
+        LDA	ACOUNT  .將A還原
+
+        ADD	#1      .A加一
+
 
         COMP    #10
         JLT	IN
+
         J	PNL
 
 
 ... PRINT
-PRINT   RMO	A,B
+PRINT   RMO	A,B     .把A放到B
         DIV	#10
         COMP	#0
         JGT     TWO	
         J       ONE
 
-ONE     JSUB	PS
+ONE     STL	PUTL
         JSUB	PS
+        JSUB	PS
+        LDL	PUTL
         RMO	B, A
         ADD	#48
         WD	OUTDEV
-        J       BACK
+        RSUB
 
-TWO     JSUB	PS
+TWO     STL	PUTL
+        JSUB	PS
+        LDL	PUTL
         ADD	#48
         WD	OUTDEV
         SUB	#48
@@ -48,11 +80,13 @@ TWO     JSUB	PS
         RMO	B,A
         ADD	#48
         WD	OUTDEV        	
-        J       BACK
+        RSUB
 
 ...PRINT NEWLINE
-PNL     LDA	NL
+PNL     STA	PUTA_1
+        LDA	NL
         WD	OUTDEV
+        LDA	PUTA_1
         J	SET
 
 ...PRINT SPACE
@@ -61,7 +95,9 @@ PS      STA	PUTA_1
         WD	OUTDEV
         LDA	PUTA_1
         RSUB
-
+        
+...OUT
+OUT     LDX	#0
 
 ...   ---------------------
 ...  //   M E M O R Y   //
@@ -69,9 +105,10 @@ PS      STA	PUTA_1
 
 OUTDEV  BYTE    X'F2'
 
-PUTA    RESW    2
+ACOUNT  RESW    2
 PUTA_1  RESW    2
 PUTS    RESW    2
+PUTL    RESW    2
 RESULT  RESW    2
 
 NINE	WORD	9
@@ -80,3 +117,6 @@ FE      WORD    48
 
 NL      WORD    10
 SPACE   WORD    32
+
+
+        END MAIN
