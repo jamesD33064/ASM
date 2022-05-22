88478702525a7095b57ce29cfc88c91e514404c9
jamesD33064 <james0120160379@gmail.com>
2022-05-23 00:58:58 +0800

    .

---
+++ SystemProgram110-2/HW_5/main_1.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+typedef struct test{
+    char CS[10];
+    char SN[10];
+    int ADR;
+    int len;
+}test;
+int main(int argc, char* argv[]) {
+    printf("CS\tSN\tADR\tLEN\n");
+    FILE* file[10];
+    test list[100];
+    int count=0;
+    int start = 4096;
+    for(int i=1;i<argc;i++){
+        file[i]=fopen(argv[i], "r");
+        char buf[128];
+        char first;
+        char *p;
+        char progname[128];int s;int size;
+        for(int y=0;y<10;y++){
+            memset(list[y].CS,'\0',10);
+            memset(list[y].SN,'\0',10);
+            list[y].len=0;
+            list[y].ADR=0;
+        }
+        while(!feof(file[i])&&fscanf(file[i],"%c",&first)!=0){
+            switch(first){
+            case 'H':
+                fscanf(file[i],"%06s%06x%06x",list[count].CS,&s,&list[count].len);
+                printf("%-06s\t%s\t%06x\n",list[count].CS,"\t",list[count].len);
+                size=list[count].len;
+                count++;
+            case 'D':
+                fscanf(file[i],"%06s%06x",list[count].SN,&list[count].ADR);
+                list[count].ADR+=start;
+                p = strtok(list[count].SN, "D");
+                printf("\t%s\t%06x\n",p,list[count].ADR);
+                count++;
+                fscanf(file[i],"%06s%06x",list[count].SN,&list[count].ADR);
+                list[count].ADR+=start;
+                printf("\t%-06s\t%06x\n",list[count].SN,list[count].ADR);
+                count++;
+                
+            default:;
+            }
+        }
+        
+        start+=size;
+    }
+}
